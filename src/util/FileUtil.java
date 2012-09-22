00515c60410e66b3bed224a2b9b11cbad51c74eb
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-09-21 20:42:41 -0700

    Skeletons of some functions for FileUtil.

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -1,5 +1,38 @@
 package util;
 
-public class FileUtil {
+import java.io.File;
+import java.util.List;
 
+public class FileUtil {
+	
+	/**
+	 * Returns a list of all files under the specified directory.  Symbolic links are not followed.
+	 * @param directory the directory to inventory.
+	 * @return a list of all files under the specified directory.
+	 */
+	public static List<File> fileInventory(File directory) {
+		return null;
+	}
+	
+	/**
+	 * Returns a list of all files under the specified directory.  Symbolic links are not followed.  
+	 * @param directory
+	 * @param includeExtentions a list of the file extentions to 
+	 * @param excludeExtentions
+	 * @return
+	 */
+	public static List<File> fileInventory(File directory, List<String> includeExtentions, List<String> excludeExtentions) {
+		return null;
+	}
+	
+	/**
+	 * Returns a list of all directories in the specified directory.  Symbolic links are ignored.
+	 * @param directory the directory to inventory.
+	 * @return a list of all directories in the specified directory.
+	 */
+	public static List<File> directoryInventory(File directory) {
+		return null;
+	}
+	
+	
 }
