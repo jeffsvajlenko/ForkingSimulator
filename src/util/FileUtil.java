cc7ce560bb6444dd9d6353efa13b47c966627fef
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-09-21 23:43:51 -0700

    Implemented fileInventory and directoryInventory in FileUtils.  Removed the fileInventory variant that takes in include/exclude file extensions (maybe will be added later).  Created unit test for these two functions (which also includes sample data).

--- src/util/FileUtil.java
+++ src/util/FileUtil.java
@@ -1,38 +1,107 @@
 package util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 
+ * A collection of utility functions for file IO.
+ *
+ */
 public class FileUtil {
 	
 	/**
-	 * Returns a list of all files under the specified directory.  Symbolic links are not followed.
+	 * Returns a list of all files under the specified directory.  Symbolic links are neither included in the results, or followed in the search.
 	 * @param directory the directory to inventory.
-	 * @return a list of all files under the specified directory.
+	 * @return a list of all files under the specified directory.  Each is specified by its absolute path.
+	 * @throws IOException if an IOException occurs.
+	 * @throws FileNotFoundException If the specified directory does not exist.
+	 * @throws IllegalArgumentException If the specified path points to a file not a directory.
 	 */
-	public static List<Path> fileInventory(Path directory) {
-		return null;
+	public static List<Path> fileInventory(Path directory) throws IOException {
+		if(!Files.exists(directory)) {
+			throw new FileNotFoundException("Specified directory does not exist.");
+		}
+		if(!Files.isDirectory(directory)) {
+			throw new IllegalArgumentException("Specified path is not a directory.");
+		}
+		InventoryFiles visitor = new InventoryFiles();
+		EnumSet<FileVisitOption> opts = EnumSet.noneOf(FileVisitOption.class);
+		Files.walkFileTree(directory, opts, Integer.MAX_VALUE, visitor);
+		return visitor.getInventory();
+	}
+	public static class InventoryFiles extends SimpleFileVisitor<Path> {
+		List<Path> inventory;
+		
+		InventoryFiles() {
+			inventory = new LinkedList<Path>();
+		}
+		
+		public List<Path> getInventory() {
+			return this.inventory;
+		}
+		
+		@Override
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+			if (!attr.isSymbolicLink() && attr.isRegularFile()) {
+				inventory.add(file.toAbsolutePath());
+			}
+			return FileVisitResult.CONTINUE;
+		}
 	}
 	
 	/**
-	 * Returns a list of all files under the specified directory.  Symbolic links are not followed.  
-	 * @param directory
-	 * @param includeExtentions a list of the file extentions to 
-	 * @param excludeExtentions
-	 * @return
-	 */
-	public static List<Path> fileInventory(Path directory, List<String> includeExtentions, List<String> excludeExtentions) {
-		return null;
-	}
-	
-	/**
-	 * Returns a list of all directories in the specified directory.  Symbolic links are ignored.
+	 * Returns a list of all directories in the specified directory.  Symbolic links are neither included in the results, or followed in the search.
 	 * @param directory the directory to inventory.
-	 * @return a list of all directories in the specified directory.
+	 * @return a list of all directories in the specified directory.  Each is specified by its absolute path.
+	 * @throws IOException if an IOException occurs.
+	 * @throws FileNotFoundException If the specified directory does not exist.
+	 * @throws IllegalArgumentException If the specified path points to a file not a directory.
 	 */
-	public static List<Path> directoryInventory(Path directory) {
-		return null;
+	public static List<Path> directoryInventory(Path directory) throws IOException {
+		if(!Files.exists(directory)) {
+			throw new FileNotFoundException("Specified directory does not exist.");
+		}
+		if(!Files.isDirectory(directory)) {
+			throw new IllegalArgumentException("Specified path is not a directory.");
+		}
+		InventoryDirectories visitor = new InventoryDirectories(directory);
+		EnumSet<FileVisitOption> opts = EnumSet.noneOf(FileVisitOption.class);
+		Files.walkFileTree(directory, opts, Integer.MAX_VALUE, visitor);
+		return visitor.getInventory();
 	}
-	
+	//Class used by directoryInventory to collect all found directories which are not symbolic links.
+	public static class InventoryDirectories extends SimpleFileVisitor<Path> {
+		List<Path> inventory;
+		Path root;
+		
+		InventoryDirectories(Path root) {
+			inventory = new LinkedList<Path>();
+			this.root = root.toAbsolutePath();
+		}
+		
+		public List<Path> getInventory() {
+			return this.inventory;
+		}
+		
+		@Override
+		public FileVisitResult preVisitDirectory(Path file, BasicFileAttributes attr) {
+			if (!attr.isSymbolicLink() && attr.isDirectory()) {
+				if(!file.toAbsolutePath().equals(root)) {
+					inventory.add(file.toAbsolutePath());
+				}
+			}
+			return FileVisitResult.CONTINUE;
+		}
+	}
 	
 }
---
+++ test/util/FileUtilTest.java
@@ -0,0 +1,86 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+public class FileUtilTest {
+
+	@Test
+	public void testFileInventory() throws IOException {
+		Path path = Paths.get("testdata/testInventory");
+		List<Path> filelist = FileUtil.fileInventory(path);
+		Collections.sort(filelist);
+		
+		//are the expected files there?
+		assertTrue(filelist.size() == 5);
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir1/file").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2/dira/file").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2/dirb/file").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2/file").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/file").toAbsolutePath().normalize()));
+		
+		//are the expected files missing?
+		assertFalse(filelist.contains(Paths.get("/home/jeff/Development/java/workspace/ForkingSimulator/testdata/testInventory/badfile")));
+		
+		//check exceptions
+		boolean exception_thrown = false;
+		try {
+			FileUtil.fileInventory(Paths.get("testdata/testInventory/dir1/file"));
+		} catch (IllegalArgumentException e) {
+			exception_thrown = true;
+		}
+		assertTrue(exception_thrown);
+		
+		exception_thrown = false;
+		try {
+			FileUtil.fileInventory(Paths.get("testdata/nosuchdirectory"));
+		} catch (FileNotFoundException e) {
+			exception_thrown = true;
+		}
+		assertTrue(exception_thrown);
+		
+	}
+
+	@Test
+	public void testDirectoryInventory() throws IOException {
+		Path path = Paths.get("testdata/testInventory");
+		List<Path> filelist = FileUtil.directoryInventory(path);
+		Collections.sort(filelist);
+		
+		//are the expected files there?
+		assertTrue(filelist.size() == 4);
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir1").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2/dira").toAbsolutePath().normalize()));
+		assertTrue(filelist.contains(Paths.get("testdata/testInventory/dir2/dirb").toAbsolutePath().normalize()));
+		
+		//are the expected files missing?
+		assertFalse(filelist.contains(Paths.get("/home/jeff/Development/java/workspace/ForkingSimulator/testdata/testInventory/badfolder")));
+		
+		//check exceptions
+		boolean exception_thrown = false;
+		try {
+			FileUtil.fileInventory(Paths.get("testdata/testInventory/dir1/file"));
+		} catch (IllegalArgumentException e) {
+			exception_thrown = true;
+		}
+		assertTrue(exception_thrown);
+		
+		exception_thrown = false;
+		try {
+			FileUtil.fileInventory(Paths.get("testdata/nosuchdirectory"));
+		} catch (FileNotFoundException e) {
+			exception_thrown = true;
+		}
+		assertTrue(exception_thrown);
+	}
+
+}
