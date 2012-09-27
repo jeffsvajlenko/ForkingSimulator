76f84ace0fe7060f19d648d841cd81f500ddb19a
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-09-27 00:35:58 -0700

    Implemented Properties for parsing a properties file for the forking simulation.  Includes unit test and test files.

--- src/Properties.java
+++ src/Properties.java
@@ -1,4 +1,288 @@
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Scanner;
+
 
 public class Properties {
+	
+	private Path system;
+	private boolean setsystem=false;
+	
+	private Path repository;
+	private boolean setrepository=false;
+	
+	private String language;
+	private boolean setlanguage=false;
+	
+	private int numfiles;
+	private boolean setnumfiles=false;
+	
+	private int numdirectories;
+	private boolean setnumdirectories=false;
+	
+	private int numfragments;
+	private boolean setnumfragments=false;
+	
+	private int maxinjectnum;
+	private boolean setmaxinjectnum=false;
+	
+	private int numforks;
+	private boolean setnumforks=false;
+	
+	private double mutationrate;
+	private boolean setmutationrate=false;
+	
+	/**
+	 * Creates a Properties object with the properties specified by the properties file.
+	 * @param propertiesfile Path to the properties file.
+	 * @throws FileNotFoundException If the properties file is not found.
+	 * @throws IllegalArgumentException If the properties file is invalid (does not exist / is a directory), or if an entry in the properties is invalid/malformed.
+	 */
+	public Properties(Path propertiesfile) {
+		Objects.requireNonNull(propertiesfile);
+		if(!Files.exists(propertiesfile)) {
+			throw new IllegalArgumentException("Propertiesfile must refer to an existing file.");
+		}
+		if(!Files.isRegularFile(propertiesfile)) {
+			throw new IllegalArgumentException("Propertiesfile must refer to a regular file.");
+		}
+		
+		Scanner s = null;
+		try {	
+			s = new Scanner(propertiesfile.toFile());
+			String line;
+			while(s.hasNextLine()) {
+				line = s.nextLine();
+				line = line.trim();
+				
+				//Comment Line
+				if(line.startsWith("#")) {
+					continue;
+					
+				//Property Line
+				} else {
+					//System Directory
+					if(line.startsWith("system=")) {
+						line = line.substring(7);
+						try {
+							this.system = Paths.get(line).toAbsolutePath().normalize();
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'system' is invalid.");
+						}
+						this.setsystem=true;
+					//Repository Directory
+					} else if (line.startsWith("repository=")) {
+						line = line.substring(11);
+						try {
+							this.repository = Paths.get(line).toAbsolutePath().normalize();
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'repository' is invalid.");
+						}
+						this.setrepository=true;
+					//Language
+					} else if (line.startsWith("language=")) {
+						line = line.substring(9).toLowerCase();
+						this.language = line;
+						if(!(language.equals("java") || language.equals("c") || language.equals("c#"))) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'language' is invalid.");
+						}
+						this.setlanguage=true;
+					//numdirectories
+					} else if (line.startsWith("numdirectories=")) {
+						line = line.substring(15);
+						try {
+							this.numdirectories = Integer.parseInt(line);
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numdirectories' is invalid.");
+						}
+						if(this.numdirectories < 0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numdirectories' is invalid.");
+						}
+						this.setnumdirectories=true;
+					//numfiles
+					} else if (line.startsWith("numfiles=")) {
+						line = line.substring(9);
+						try {
+							this.numfiles = Integer.parseInt(line);
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numfiles' is invalid.");
+						}
+						if(numfiles < 0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numdirectories' is invalid.");
+						}
+						this.setnumfiles=true;
+					//numfragments
+					} else if (line.startsWith("numfragments=")) {
+						line = line.substring(13);
+						try {
+							this.numfragments = Integer.parseInt(line);
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numfragments' is invalid.");
+						}
+						if(this.numfragments < 0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numfragments' is invalid.");
+						}
+						this.setnumfragments=true;
+					//numforks
+					} else if (line.startsWith("numforks=")) {
+						line = line.substring(9);
+						try {
+							this.numforks = Integer.parseInt(line);
+						} catch (Exception e){
+							s.close();
+							throw new IllegalArgumentException("Propety 'numforks' is invalid.");
+						}
+						if(this.numforks <= 0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'numforks' is invalid.");
+						}
+						this.setnumforks=true;
+					//maxinjectnum
+					} else if (line.startsWith("maxinjectnum=")) {
+						line = line.substring(13);
+						try {
+							this.maxinjectnum = Integer.parseInt(line);
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'maxinjectnum' is invalid.");
+						}
+						if(this.maxinjectnum <= 0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'maxinjectnum' is invalid.");
+						}
+						this.setmaxinjectnum=true;
+					//mutation rate
+					} else if (line.startsWith("mutationrate=")) {
+						line = line.substring(13);
+						try {
+							this.mutationrate = Double.parseDouble(line);
+						} catch (Exception e) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'mutationrate' is invalid.");
+						}
+						if(this.mutationrate < 0.0 || this.mutationrate > 1.0) {
+							s.close();
+							throw new IllegalArgumentException("Propety 'mutationrate' is invalid.");
+						}
+						this.setmutationrate=true;
+					}
+				}
+			}
+			//Check properties were set
+			if(!this.setlanguage) {
+				s.close();
+				throw new IllegalArgumentException("Property 'language' was not specified.");
+			}
+			if(!this.setmaxinjectnum) {
+				s.close();
+				throw new IllegalArgumentException("Property 'maxinjectnum' was not specified.");
+			}
+			if(!this.setmutationrate) {
+				s.close();
+				throw new IllegalArgumentException("Property 'mutationrate' was not specified.");
+			}
+			if(!this.setnumdirectories) {
+				s.close();
+				throw new IllegalArgumentException("Property 'numdirectories' was not specified.");
+			}
+			if(!this.setnumfiles) {
+				s.close();
+				throw new IllegalArgumentException("Property 'numfiles' was not specified.");
+			}
+			if(!this.setnumforks) {
+				s.close();
+				throw new IllegalArgumentException("Property 'numforks' was not specified.");
+			}
+			if(!this.setnumfragments) {
+				s.close();
+				throw new IllegalArgumentException("Property 'numfragments' was not specified.");
+			}
+			if(!this.setrepository) {
+				s.close();
+				throw new IllegalArgumentException("Property 'repository' was not specified.");
+			}
+			if(!this.setsystem) {
+				s.close();
+				throw new IllegalArgumentException("Property 'system' was not specified.");
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Propertiesfile must refer to an existing file.");
+		}
+	}
 
+	/**
+	 * @return The path to the system being forked.
+	 */
+	public Path getSystem() {
+		return system;
+	}
+
+	/**
+	 * @return The path to the repository where variants are mined form.
+	 */
+	public Path getRepository() {
+		return repository;
+	}
+
+	/**
+	 * @return The source language of the system and repository.
+	 */
+	public String getLanguage() {
+		return language;
+	}
+
+	/**
+	 * @return The number of file variants to create.
+	 */
+	public int getNumfiles() {
+		return numfiles;
+	}
+
+	/**
+	 * @return The number of directory variants to create.
+	 */
+	public int getNumdirectories() {
+		return numdirectories;
+	}
+
+	/**
+	 * @return The number of fragment variants to create.
+	 */
+	public int getNumfragments() {
+		return numfragments;
+	}
+
+	/**
+	 * @return The maximum number of forked systems the variants should be duplicated in.
+	 */
+	public int getMaxinjectnum() {
+		return maxinjectnum;
+	}
+
+	/**
+	 * @return The number of forked systems to create.
+	 */
+	public int getNumforks() {
+		return numforks;
+	}
+
+	/**
+	 * @return The chance that a fragment variant is mutated before injection into a fork.
+	 */
+	public double getMutationrate() {
+		return mutationrate;
+	}
 }
---
+++ test/PropertiesTest.java
@@ -0,0 +1,237 @@
+import static org.junit.Assert.*;
+
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+
+public class PropertiesTest {
+
+	@Test
+	public void testProperties() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(Paths.get("/path/to/system/").toAbsolutePath().normalize(), p.getSystem());
+		assertEquals(Paths.get("/path/to/repository/").toAbsolutePath().normalize(), p.getRepository());
+		assertEquals("java", p.getLanguage());
+		assertEquals(10, p.getNumfiles());
+		assertEquals(15, p.getNumdirectories());
+		assertEquals(5, p.getNumfragments());
+		assertEquals(2, p.getMaxinjectnum());
+		assertEquals(11, p.getNumforks());
+		assertEquals(0.25, p.getMutationrate(), 0.0001);
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(Paths.get("/path/to/folder/system/").toAbsolutePath().normalize(), p.getSystem());
+		assertEquals(Paths.get("/path/to/other/repository/").toAbsolutePath().normalize(), p.getRepository());
+		assertEquals("c", p.getLanguage());
+		assertEquals(2, p.getNumfiles());
+		assertEquals(4, p.getNumdirectories());
+		assertEquals(6, p.getNumfragments());
+		assertEquals(8, p.getMaxinjectnum());
+		assertEquals(10, p.getNumforks());
+		assertEquals(0.75, p.getMutationrate(), 0.0001);
+		
+		boolean caught;
+		
+		//language missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/languagemissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'language' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//maxinject mising
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/maxinjectnummissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'maxinjectnum' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//mutationrate misssing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/mutationratemissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'mutationrate' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//numdirectories missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/numdirectoriesmissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'numdirectories' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//numfiles missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/numfilesmissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'numfiles' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//numforks missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/numforksmissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'numforks' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//numfragments missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/numfragmentsmissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'numfragments' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//repository missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/repositorymissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'repository' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//system missing
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/systemmissing"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Property 'system' was not specified."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//null parameter
+		caught = false;
+		try {
+			p = new Properties(null);
+		} catch (NullPointerException e) {
+			caught = true;
+		}
+		assertTrue(caught);
+		
+		//file does not exist
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/nosuchfile"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Propertiesfile must refer to an existing file."))
+				caught = true;
+		}
+		assertTrue(caught);
+		
+		//not regular file
+		caught = false;
+		try {
+			p = new Properties(Paths.get("testdata/testproperties/"));
+		} catch (IllegalArgumentException e) {
+			if(e.getMessage().equals("Propertiesfile must refer to a regular file."))
+				caught = true;
+		}
+		assertTrue(caught);
+	}
+
+	@Test
+	public void testGetSystem() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(Paths.get("/path/to/system/").toAbsolutePath().normalize(), p.getSystem());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(Paths.get("/path/to/folder/system/").toAbsolutePath().normalize(), p.getSystem());
+	}
+
+	@Test
+	public void testGetRepository() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(Paths.get("/path/to/repository/").toAbsolutePath().normalize(), p.getRepository());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(Paths.get("/path/to/other/repository/").toAbsolutePath().normalize(), p.getRepository());
+	}
+
+	@Test
+	public void testGetLanguage() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals("java", p.getLanguage());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals("c", p.getLanguage());
+	}
+
+	@Test
+	public void testGetNumfiles() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(10, p.getNumfiles());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(2, p.getNumfiles());
+	}
+
+	@Test
+	public void testGetNumdirectories() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(15, p.getNumdirectories());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(4, p.getNumdirectories());
+	}
+
+	@Test
+	public void testGetNumfragments() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(5, p.getNumfragments());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(6, p.getNumfragments());
+	}
+
+	@Test
+	public void testGetMaxinjectnum() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(2, p.getMaxinjectnum());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(8, p.getMaxinjectnum());
+	}
+
+	@Test
+	public void testGetNumforks() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(11, p.getNumforks());
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(10, p.getNumforks());
+	}
+
+	@Test
+	public void testGetMutationrate() {
+		Properties p = new Properties(Paths.get("testdata/testproperties/properties"));
+		assertEquals(0.25, p.getMutationrate(), 0.0001);
+		
+		p = new Properties(Paths.get("testdata/testproperties/properties2"));
+		assertEquals(0.75, p.getMutationrate(), 0.0001);
+	}
+
+}
