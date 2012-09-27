39cdf5d038eef52ca8e4ee3c4421a304802fe989
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-09-26 19:51:20 -0700

    Added classes for tracking variants added to forks.  Including JUnit tests.

---
+++ src/Variant.java
@@ -0,0 +1,36 @@
+
+public abstract class Variant {
+	//The variants type
+	private int type;
+	
+	/**
+	 * Signifies a file variant.
+	 */
+	public static final int FILE = 1;
+	
+	/**
+	 * Signifies a directory variant.
+	 */
+	public static final int DIRECTORY = 2;
+	
+	/**
+	 * Signifies a fragment variant.
+	 */
+	public static final int FRAGMENT = 3;
+
+	/**
+	 * Constructs a Variant of the specified variant type.
+	 * @param type the type of the variant.
+	 */
+	public Variant(int type) {
+		this.type = type;
+	}
+	
+	/**
+	 * Returns the variant's type.  Refer to constants in Variant for meaning.
+	 * @return the variant's type.
+	 */
+	public int getType() {
+		return this.type;
+	}
+}
