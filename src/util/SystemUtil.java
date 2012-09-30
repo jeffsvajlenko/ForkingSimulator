48edddcb01a8c7bc172e2c2284ece9a22e9be194
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-09-30 08:31:08 -0700

    Added SystemUtil test file.  Added ability to call TXL to SystemUtil.

--- src/util/SystemUtil.java
+++ src/util/SystemUtil.java
@@ -1,26 +1,49 @@
 package util;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class SystemUtil {
+	/**
+	 * 
+	 * @return
+	 */
 	public static Path getInstallRoot() {
 		return Paths.get(".").toAbsolutePath().normalize();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static Path getScriptsLocation() {
 		return getInstallRoot().resolve("scripts").toAbsolutePath().normalize();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static Path getTemporaryDirectory() {
 		return getInstallRoot().resolve("tmp").toAbsolutePath().normalize();
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public static Path getTxlDirectory() {
 		return getInstallRoot().resolve("txl").toAbsolutePath().normalize();
 	}
 	
+	/**
+	 * 
+	 * @param language
+	 * @return
+	 */
 	public static Path getTxlDirectory(String language) {
 		Path p = getTxlDirectory().resolve(language);
 		if(Files.isDirectory(p)) {
@@ -29,5 +52,61 @@ public class SystemUtil {
 		else {
 			return null;
 		}
+	}
+	
+	/**
+	 * Runs the given TXL script for the given input file and outputs the result to the output file.
+	 * The function returns once the script is complete.
+	 * @param txlScriptName The TXL script to run.
+	 * @param inputFile The input file.
+	 * @param outputFile The output file.
+	 * @return The return value of the TXL execution (0 is success, > 0 is failure).  Or -1 if TXL execution failed.
+	 */
+	public static int runTxl(Path txlScriptName, Path inputFile, Path outputFile) {
+		//Check input
+		Objects.requireNonNull(txlScriptName);
+		Objects.requireNonNull(inputFile);
+		Objects.requireNonNull(outputFile);
+		if(!Files.isReadable(txlScriptName)) {
+			throw new IllegalArgumentException("Specified txl script does not exist or is not readable (" + txlScriptName.toAbsolutePath().normalize() + ".");
+		}
+		if(!Files.isReadable(inputFile)) {
+			throw new IllegalArgumentException("Input file is not readable or does not exist.");
+		}
+		if(!Files.exists(outputFile)) {
+			try {
+				Files.createFile(outputFile);
+			} catch (IOException e) {
+				throw new IllegalArgumentException("Output file can not be written to.");
+			}
+		}
+		if(!Files.isWritable(outputFile)) {
+			throw new IllegalArgumentException("Output file can not be written to.");
+		}
+		
+		//Execute TXL
+		String command = "txl " + " -o " + outputFile.toAbsolutePath().normalize().toString() + " " + inputFile.toAbsolutePath().normalize().toString() + " " + txlScriptName.toAbsolutePath().normalize().toString();
+		Process process = null;
+		int retval;
+		try {
+			process = Runtime.getRuntime().exec(command);
+			new StreamGobbler(process.getInputStream()).start();
+			new StreamGobbler(process.getErrorStream()).start();
+			retval = process.waitFor();
+		} catch (IOException e) {
+			retval = -1;
+		} catch (InterruptedException e) {
+			retval = -1;
+		} finally {
+			if(process != null) {
+				try {process.getErrorStream().close();} catch (IOException e) {}
+				try {process.getInputStream().close();} catch (IOException e) {}
+				try {process.getOutputStream().close();} catch (IOException e) {}
+				process.destroy();
+				process = null;
+			}
+		}
+		
+		return retval;
 	}
 }
---
+++ test/util/SystemUtilTest.java
@@ -0,0 +1,42 @@
+package util;
+
+import static org.junit.Assert.*;
+
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class SystemUtilTest {
+
+	@Test
+	public void testGetInstallRoot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScriptsLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTemporaryDirectory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTxlDirectory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTxlDirectoryString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRunTxl() {
+		int retval = SystemUtil.runTxl(SystemUtil.getTxlDirectory("java").resolve("PrettyPrint.txl"), Paths.get("testdata/SystemUtilTest/InventoriedSystem.java"), Paths.get("testdata/SystemUtilTest/InventoriedSystem.java_out"));
+		assertTrue(retval==0);
+	}
+
+}
