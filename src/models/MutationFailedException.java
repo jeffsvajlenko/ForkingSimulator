345d558e22eb87afd4426ef6d985ec39f2cad417
Jeff Svajlenko <jeff.svajlenko@gmail.com>
2012-11-22 13:14:27 -0600

    Forgot to add MutationFailedException previously.

---
+++ src/models/MutationFailedException.java
@@ -0,0 +1,30 @@
+package models;
+
+public class MutationFailedException extends Exception {
+
+	public MutationFailedException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MutationFailedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MutationFailedException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MutationFailedException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MutationFailedException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
